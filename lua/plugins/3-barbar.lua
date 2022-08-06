00d8f4f5ab9f997194f1adf2b032207f29f62ced
Dax <contact@antoniodavide.dev>
2022-08-06 19:40:48 +0200

    Remapped BarBar shortcuts

--- lua/plugins/3-barbar.lua
+++ lua/plugins/3-barbar.lua
@@ -9,30 +9,15 @@ common.set_options("g", {
     }
 })
 
--- common.map("n", "<Leader>1", ":BufferGoto 1<CR>")
--- common.map("n", "<Leader>2", ":BufferGoto 2<CR>")
--- common.map("n", "<Leader>3", ":BufferGoto 3<CR>")
--- common.map("n", "<Leader>4", ":BufferGoto 4<CR>")
--- common.map("n", "<Leader>5", ":BufferGoto 5<CR>")
--- common.map("n", "<Leader>6", ":BufferGoto 6<CR>")
--- common.map("n", "<Leader>7", ":BufferGoto 7<CR>")
--- common.map("n", "<Leader>8", ":BufferGoto 8<CR>")
--- common.map("n", "<Leader>9", ":BufferLast<CR>")
--- common.map("n", "<Leader>-", ":BufferClose!<CR>")
--- common.map("n", "<Leader>+", ":enew<CR>")
--- common.map("n", "<Leader>p", ":BufferPick<CR>")
-
-common.map_keys({
-    {"n", "<Leader>1", ":BufferGoto 1<CR>", {noremap = true, silent = true}},
-    {"n", "<Leader>2", ":BufferGoto 2<CR>", {noremap = true, silent = true}},
-    {"n", "<Leader>3", ":BufferGoto 3<CR>", {noremap = true, silent = true}},
-    {"n", "<Leader>4", ":BufferGoto 4<CR>", {noremap = true, silent = true}},
-    {"n", "<Leader>5", ":BufferGoto 5<CR>", {noremap = true, silent = true}},
-    {"n", "<Leader>6", ":BufferGoto 6<CR>", {noremap = true, silent = true}},
-    {"n", "<Leader>7", ":BufferGoto 7<CR>", {noremap = true, silent = true}},
-    {"n", "<Leader>8", ":BufferGoto 8<CR>", {noremap = true, silent = true}},
-    {"n", "<Leader>9", ":BufferLast<CR>",   {noremap = true, silent = true}},
-    {"n", "<Leader>-", ":BufferClose!<CR>", {noremap = true, silent = true}},
-    {"n", "<Leader>+", ":enew<CR>",         {noremap = true, silent = true}},
-    {"n", "<Leader>p", ":BufferPick<CR>",   {noremap = true, silent = true}},
-})
+common.map("<Leader>1", ":BufferGoto 1<CR>", "n")
+common.map("<Leader>2", ":BufferGoto 2<CR>", "n")
+common.map("<Leader>3", ":BufferGoto 3<CR>", "n")
+common.map("<Leader>4", ":BufferGoto 4<CR>", "n")
+common.map("<Leader>5", ":BufferGoto 5<CR>", "n")
+common.map("<Leader>6", ":BufferGoto 6<CR>", "n")
+common.map("<Leader>7", ":BufferGoto 7<CR>", "n")
+common.map("<Leader>8", ":BufferGoto 8<CR>", "n")
+common.map("<Leader>9", ":BufferLast<CR>", "n")
+common.map("<Leader>-", ":BufferClose!<CR>", "n")
+common.map("<Leader>+", ":enew<CR>", "n")
+common.map("<Leader>p", ":BufferPick<CR>", "n")
