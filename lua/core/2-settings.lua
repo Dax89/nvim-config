f7dcdcbdb3d2fd7fb16e2fc07c8b95859eb99259
Dax <contact@antoniodavide.dev>
2022-08-07 18:56:08 +0200

    Hide some buffers from tabs

--- lua/core/2-settings.lua
+++ lua/core/2-settings.lua
@@ -8,7 +8,7 @@ common.set_options("g", {
         maximum_padding = 1,
         auto_hide = false,
         icons = "both",
-        exclude_ft = {"qf", "dap-repl"} -- Hide some windows
+        exclude_ft = {"qf", "dap-repl", "fugitive", "gitcommit"} -- Hide some windows
     }
 })
 
