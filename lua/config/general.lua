e33b62b326d05ce7325e91a3c935bca492a3c5fc
Dax <contact@antoniodavide.dev>
2022-08-08 11:09:32 +0200

    Added "LSP Restart" Quick Action

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -4,6 +4,7 @@ local function show_general_settings()
     common.show_select("Quick Actions", {
         {" Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k"},
         {" Sync Plugins", ":PackerSync"},
+        {"↺ Restart LSP", ":LspRestart"},
         {"! Mason", ":Mason"},
         {"⚙ Commands", function() require("telescope.builtin").commands() end}
     })
