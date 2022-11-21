7c58b9404109d159d6135d012663abfd151f3b4b
Dax <contact@antoniodavide.dev>
2022-11-21 17:25:38 +0100

    Improved completion sorting

--- lua/plugins/5-cmp.lua
+++ lua/plugins/5-cmp.lua
@@ -79,15 +79,19 @@ cmp.setup({
     }),
 
     sorting           = {
+        priority_weight = 1.0,
+
         comparators = {
-            cmp.config.compare.offset,
-            cmp.config.compare.exact,
+            cmp.config.compare.locality,
             cmp.config.compare.recently_used,
             require("clangd_extensions.cmp_scores"),
-            cmp.config.compare.kind,
-            cmp.config.compare.sort_text,
-            cmp.config.compare.length,
+            cmp.config.compare.score,
+            cmp.config.compare.offset,
             cmp.config.compare.order,
+            -- cmp.config.compare.sort_text,
+            -- cmp.config.compare.exact,
+            -- cmp.config.compare.kind,
+            -- cmp.config.compare.length,
         },
     },
 })
