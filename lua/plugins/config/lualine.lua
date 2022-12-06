dd1af959cc5ba2dfe8b159cf64730e3190e0bd08
Dax <contact@antoniodavide.dev>
2022-12-06 13:03:37 +0100

    LuaLine: Enable in terminal too

--- lua/plugins/config/lualine.lua
+++ lua/plugins/config/lualine.lua
@@ -1,7 +1,11 @@
+local function filter_filetypes(ft)
+    return not vim.tbl_contains({"toggleterm"}, ft)
+end
+
 require("lualine").setup({
     options = {
         theme = "tundra",
-        disabled_filetypes = require("config.common").filetype_blacklist
+        disabled_filetypes = vim.tbl_filter(filter_filetypes, require("config.common").filetype_blacklist)
     },
     integration = {nvimtree = {enabled = true, show_root = true}}
 })
