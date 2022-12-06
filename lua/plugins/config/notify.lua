cd32dcdf83f3387c3b164035cda533e6210dc3c4
Dax <contact@antoniodavide.dev>
2022-12-06 14:31:41 +0100

    vim.notify: Updated config

--- lua/plugins/config/notify.lua
+++ lua/plugins/config/notify.lua
@@ -1,1 +1,10 @@
-vim.notify = require("notify")
+local notify = require("notify")
+
+notify.setup({
+    stages = "static",
+    top_down = false,
+    timeout = 2000,
+    fps = 60,
+})
+
+vim.notify = notify
