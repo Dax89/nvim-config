e1d35009d909c8dc77e2140295421d14d508a7bd
Dax <contact@antoniodavide.dev>
2023-02-22 16:51:06 +0100

    DAP: Updated mappings

--- lua/plugins/mappings/dap.lua
+++ lua/plugins/mappings/dap.lua
@@ -2,6 +2,8 @@ vim.keymap.set("n", "<F9>", function() require("dap").toggle_breakpoint() end)
 vim.keymap.set("n", "<F10>", function() require("dap").step_over() end)
 vim.keymap.set("n", "<F11>", function() require("dap").step_into() end)
 
+vim.keymap.set("n", "<leader>db", function() require("dap").list_breakpoints() end)
+vim.keymap.set("n", "<leader>dC", function() require("dap").clear_breakpoints() end)
 vim.keymap.set("n", "<leader>dc", function() require("dap").continue() end)
 vim.keymap.set("n", "<leader>de", function() require("dap").set_exception_breakpoints({"all"}) end)
 vim.keymap.set("n", "<leader>di", function() require("dap.ui.widgets").hover() end)
@@ -19,7 +21,6 @@ vim.keymap.set("n", "<leader>ds", function() require("dap.ui.widgets").centered_
 vim.keymap.set("v", "<leader>di", function() require("dap.ui.widgets").hover() end)
 vim.keymap.set("v", "<leader>dp", function() require("dap.ui.widgets").preview() end)
 
-vim.keymap.set("n", "<leader>db", function() require("telescope").extensions.dap.list_breakpoints() end)
 vim.keymap.set("n", "<leader>dv", function() require("telescope").extensions.dap.variables() end)
 vim.keymap.set("n", "<leader>df", function() require("telescope").extensions.dap.frames() end)
 
