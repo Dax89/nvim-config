ae3120e7387e8ba8fa827c1ebf8db21416636c41
Dax <contact@antoniodavide.dev>
2023-03-04 17:08:26 +0100

    Added Harpoon and Refactoring plugins, improved keybinds

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,15 @@
+return {
+    "ThePrimeagen/harpoon",
+    config = true,
+
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+    },
+
+    keys = {
+        {"<leader>hh", function() require("harpoon.ui").toggle_quick_menu() end},
+        {"<leader>ha", function() require("harpoon.mark").add_file() end},
+        {"<leader>hp", function() require("harpoon.ui").nav_prev() end},
+        {"<leader>hn", function() require("harpoon.ui").nav_next() end}
+    },
+}
