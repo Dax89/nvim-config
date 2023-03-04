f179c82151edde9d843e672b7453d18cafb19cf3
Dax <contact@antoniodavide.dev>
2023-03-04 19:12:42 +0100

    Pin Colorscheme for now

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
     {
         "rebelot/kanagawa.nvim",
+        commit = "de7fb5f5de25ab45ec6039e33c80aeecc891dd92",
 
         config = function()
             local common = require("config.common")
