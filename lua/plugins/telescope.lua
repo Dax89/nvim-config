ef08882fa524e2d220c1fd6b8c4f5758cbbe5d4f
Dax <contact@antoniodavide.dev>
2023-11-07 14:55:38 +0100

    fix(Telescope): silence error if not in git repo

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,7 +21,7 @@ return {
             { "<leader>b", "<CMD>Telescope buffers<CR>",                       desc = "Telescope - Buffers" },
             { "<C-h>",     "<CMD>Telescope oldfiles<CR>",                      desc = "Telescope - History" },
             { "<C-k>",     "<CMD>Telescope find_files<CR>",                    desc = "Telescope = Find Files" },
-            { "<C-p>",     "<CMD>Telescope git_files<CR>",                     desc = "Telescope = Git Files" },
+            { "<C-p>",     "<CMD>silent! Telescope git_files<CR>",             desc = "Telescope = Git Files" },
             { "<C-g>",     "<CMD>Telescope live_grep<CR>",                     desc = "Telescope = Grep Files" },
             { "<C-s>",     "<CMD>Telescope lsp_document_symbols<CR>",          desc = "Telescope - LSP Outline" },
             { "<C-Enter>", "<CMD>Telescope lsp_references<CR>",                desc = "Telescope - LSP References" },
