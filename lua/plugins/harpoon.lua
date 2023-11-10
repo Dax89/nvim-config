777cfcb8b21b21994f1b37ce095966a4d7f786ce
Dax <contact@antoniodavide.dev>
2023-11-10 12:09:57 +0100

    feat(Harpoon): improved mappings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,20 +6,32 @@ return {
         "nvim-telescope/telescope.nvim",
     },
 
-    keys = {
-        { "<leader>hh", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon - Open" },
-        { "<leader>ha", function() require("harpoon.mark").add_file() end,        desc = "Harpoon - Mark" },
-        { "<leader>hp", function() require("harpoon.ui").nav_prev() end,          desc = "Harpoon = Prev" },
-        { "<leader>hn", function() require("harpoon.ui").nav_next() end,          desc = "Harpoon - Next" },
-        { "<leader>h1", function() require("harpoon.ui").nav_file(1) end,         desc = "Harpoon - Bookmark 1" },
-        { "<leader>h2", function() require("harpoon.ui").nav_file(2) end,         desc = "Harpoon - Bookmark 2" },
-        { "<leader>h3", function() require("harpoon.ui").nav_file(3) end,         desc = "Harpoon - Bookmark 3" },
-        { "<leader>h4", function() require("harpoon.ui").nav_file(4) end,         desc = "Harpoon - Bookmark 4" },
-        { "<leader>h5", function() require("harpoon.ui").nav_file(5) end,         desc = "Harpoon - Bookmark 5" },
-        { "<leader>h6", function() require("harpoon.ui").nav_file(6) end,         desc = "Harpoon - Bookmark 6" },
-        { "<leader>h7", function() require("harpoon.ui").nav_file(7) end,         desc = "Harpoon - Bookmark 7" },
-        { "<leader>h8", function() require("harpoon.ui").nav_file(8) end,         desc = "Harpoon - Bookmark 8" },
-        { "<leader>h9", function() require("harpoon.ui").nav_file(9) end,         desc = "Harpoon - Bookmark 9" },
-        { "<leader>h0", function() require("harpoon.ui").nav_file(10) end,        desc = "Harpoon - Bookmark 10" },
-    },
+    keys = function()
+        local tbl = {
+            { "<leader>hh", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon - Open" },
+            { "H",          function() require("harpoon.ui").nav_prev() end,          desc = "Harpoon = Prev" },
+            { "L",          function() require("harpoon.ui").nav_next() end,          desc = "Harpoon - Next" },
+            {
+                "<leader>ha",
+                function()
+                    require("harpoon.mark").add_file()
+
+                    local common = require("config.common")
+                    local filename = common.get_filename(vim.api.nvim_buf_get_name(0))
+                    vim.notify("'" .. filename .. "' added", "info", { title = "Harpoon" })
+                end,
+                desc = "Harpoon - Add"
+            }
+        }
+
+        for i = 1, 10 do
+            table.insert(tbl, {
+                "<leader>" .. (i % 10),
+                function() require("harpoon.ui").nav_file(i) end,
+                desc = "Harpoon - Bookmark " .. i,
+            })
+        end
+
+        return tbl
+    end
 }
