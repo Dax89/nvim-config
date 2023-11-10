5df6e8f04e936949735a6bdc847cf9c0620aba1f
Dax <contact@antoniodavide.dev>
2023-11-10 16:02:05 +0100

    chore: removed numb

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -85,17 +85,6 @@ return {
     },
 
     {
-        "nacro90/numb.nvim",
-
-        opts = {
-            show_numbers = true,
-            show_cursorline = true,
-            number_only = false,
-            centered_peeking = true,
-        }
-    },
-
-    {
         "mbbill/undotree",
 
         init = function()
