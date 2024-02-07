74d1beb1a8549582ee6a73c03305dc7a64eac499
Dax <contact@antoniodavide.dev>
2024-02-07 16:19:25 +0100

    feat(ansi16): split palette

---
+++ lua/config/ansi16/palette.lua
@@ -0,0 +1,26 @@
+local lush = require("lush")
+local hsl = lush.hsl
+
+local p = {
+    -- Regular
+    color0  = hsl("#202020"), -- Black
+    color1  = hsl("#ff7575"), -- Red
+    color2  = hsl("#67bd61"), -- Green
+    color3  = hsl("#ffb22e"), -- Yellow
+    color4  = hsl("#88b0d7"), -- Blue
+    color5  = hsl("#e28ee6"), -- Magenta
+    color6  = hsl("#13c3a0"), -- Cyan
+    color7  = hsl("#cacaca"), -- White
+
+    -- Bright
+    color8  = hsl("#757575"), -- Black
+    color9  = hsl("#ffa3a3"), -- Red
+    color10 = hsl("#96d952"), -- Green
+    color11 = hsl("#fff061"), -- Yellow
+    color12 = hsl("#90caf9"), -- Blue
+    color13 = hsl("#f8c7ff"), -- Magenta
+    color14 = hsl("#4ce0c5"), -- Cyan
+    color15 = hsl("#f4f1d6"), -- White
+}
+
+return p
