8c21c1e28617007f5d6ba0785353669521a810c9
Dax <contact@antoniodavide.dev>
2024-04-01 22:30:11 +0200

    feat: removed vim-easymotion

--- lua/plugins/vim.lua
+++ lua/plugins/vim.lua
@@ -13,23 +13,4 @@ return {
             { "<C-f>", "<CMD>Vifm<CR>", desc = "VIFM - Open" }
         },
     },
-    {
-        "easymotion/vim-easymotion",
-
-        init = function()
-            vim.g.EasyMotion_keys = "hjklasdfgyuiopqwertnmzxcvb"
-            vim.g.EasyMotion_prompt = "Jump to → "
-            vim.g.EasyMotion_leader_key = " "
-            vim.g.EasyMotion_smartcase = 1
-            vim.g.EasyMotion_grouping = 1
-            vim.g.EasyMotion_do_mapping = 0
-            vim.g.EasyMotion_verbose = 0
-        end,
-
-        keys = {
-            { ",w", "<Plug>(easymotion-bd-w)",  desc = "EasyMotion - Find Word" },
-            { ",W", "<Plug>(easymotion-bd-W)",  desc = "EasyMotion - Find WORD" },
-            { ",f", "<Plug>(easymotion-bd-f2)", desc = "EasyMotion - Find" },
-        }
-    }
 }
